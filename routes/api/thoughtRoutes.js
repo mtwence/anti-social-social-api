649f74c72c278d3776bf2f03bd7ffd6453ea644b
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-08-18 23:19:17 -0400

    add thoughts  routing

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const {
+  getTotalThoughts,
+  getThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controller/thought-controller");
+
+router.route("/").get(getTotalThoughts).post(createThought);
+
+router.route("/:id").get(getThought).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
